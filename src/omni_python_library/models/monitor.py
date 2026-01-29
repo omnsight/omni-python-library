79d62bfaa23e5cd28ede365696a1c6a104b821c0
maxtian-xuanlan <mxt6232996077@gmail.com>
2026-01-28 23:20:29 -0800

    Fix the types of monitoring sources by enum

--- src/omni_python_library/models/monitor.py
+++ src/omni_python_library/models/monitor.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Any, Dict, Optional
 
 from pydantic import BaseModel, Field
@@ -5,13 +6,19 @@ from pydantic import BaseModel, Field
 from omni_python_library.models.common import ArangoData
 
 
+class SourceType(str, Enum):
+    WEBSITE = "website"
+    TWITTER = "twitter"
+    TELEGRAM = "telegram"
+
+
 class MonitoringSourceMainData(BaseModel):
     name: Optional[str] = Field(default=None, description="Name of the monitoring source")
-    type: Optional[str] = Field(default=None, description="Type of the monitoring source such as 'website', 'news site', 'twitter', 'telegram'")
+    type: Optional[SourceType] = Field(default=None, description="Type of the monitoring source such as 'website', 'twitter', 'telegram'")
     url: Optional[str] = Field(default=None, description="URL of the monitoring source")
     attributes: Optional[Dict[str, Any]] = Field(
         default=None,
-        description="Platform-specific identifiers or handles required to monitor the source via API calls. Examples include a Twitter username handle (e.g., 'elonmusk'), a Telegram channel ID, or a news site URL (e.g., 'https://www.cnn.com').",
+        description="Platform-specific identifiers or handles required to monitor the source via API calls. Examples include a Twitter user id or a Telegram channel ID.",
     )
 
 
