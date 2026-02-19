d82cf5f4683c2345b3ac1abc08f3898af0e11840
maxtian-xuanlan <mxt6232996077@gmail.com>
2026-02-18 23:33:35 -0800

    Add relations in query entity neighbors

--- src/omni_python_library/dal/query_tools/entity_neighborhood.py
+++ src/omni_python_library/dal/query_tools/entity_neighborhood.py
@@ -13,21 +13,27 @@ def search_entity_neighborhood(
     in_pending: bool = False,
 ) -> List[Union[Relation, Event, Source, Person, Organization, Website]]:
     """
-    Searches for all types of entities 1 edge away from the given entity ID.
+    Searches for all types of entities and relations 1 edge away from the given entity ID.
 
     :param entity_id: The ID of the entity to start the search from.
     :param limit: Maximum number of entities to return.
     :param owner: The owner of the documents to filter by.
     :param roles: The roles to filter by.
-    :return: A list of entities found 1 edge away.
+    :return: A list of entities and relations found 1 edge away.
     """
     bind_vars = {"entity_id": entity_id, "limit": limit, "owner": owner, "roles": roles}
 
     query = f"""
-    FOR v, e IN 1..1 ANY @entity_id GRAPH '{ArangoDBConstant.EVENT_RELATED_GRAPH}'
-        FILTER (v.owner == @owner OR LENGTH(INTERSECTION(v.read, @roles)) > 0)
-        LIMIT @limit
-        RETURN v
+    LET traversal = (
+        FOR v, e IN 1..1 ANY @entity_id GRAPH '{ArangoDBConstant.EVENT_RELATED_GRAPH}'
+            FILTER (v.owner == @owner OR LENGTH(INTERSECTION(v.read, @roles)) > 0)
+            LIMIT @limit
+            RETURN {{ v: v, e: e }}
+    )
+    LET vertices = (FOR t IN traversal RETURN t.v)
+    LET edges = (FOR t IN traversal RETURN t.e)
+    FOR doc IN UNION(vertices, edges)
+        RETURN doc
     """
 
     return OsintDataAccessLayer().query(query, bind_vars=bind_vars, in_pending=in_pending)
