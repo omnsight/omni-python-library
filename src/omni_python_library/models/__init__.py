b50b03eabee042b96c271e443bd84f4e763fdd25
bouncingmaxt <mxt6232996077@gmail.com>
2026-02-20 04:46:14 -0800

    Add monitor trigger main data in exports (#16)

--- src/omni_python_library/models/__init__.py
+++ src/omni_python_library/models/__init__.py
@@ -1,6 +1,6 @@
 from omni_python_library.models.common import ArangoData, LocationData, Permissive
 from omni_python_library.models.monitor import MonitoringSource, MonitoringSourceMainData, SourceType
-from omni_python_library.models.monitor_trigger import MonitorTrigger
+from omni_python_library.models.monitor_trigger import MonitorTrigger, MonitorTriggerMainData
 from omni_python_library.models.osint import (
     Event,
     EventMainData,
@@ -42,4 +42,5 @@ __all__ = [
     "ViewMode",
     "ViewUI",
     "MonitorTrigger",
+    "MonitorTriggerMainData",
 ]
