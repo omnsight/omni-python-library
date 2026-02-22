90306e71c1891895cdb492961768eca8f93e6e9b
bouncingmaxt <mxt6232996077@gmail.com>
2026-02-21 16:46:18 -0800

    Update test.yml (#22)
    * Update test.yml
    
    * Update test.yml
    
    * Auto [skip ci]: update dependencies and generated client
    
    ---------
    
    Co-authored-by: bouncingmaxt <212165011+bouncingmaxt@users.noreply.github.com>

--- src/omni_python_library/middleware/__init__.py
+++ src/omni_python_library/middleware/__init__.py
@@ -1,8 +1,8 @@
 from omni_python_library.middleware.user_token import (
     get_current_user,
     get_owner_from_token,
-    get_user_roles,
     get_user_context,
+    get_user_roles,
     validate_create_permission,
 )
 
