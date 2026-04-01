d239c8e706859552993286062014e07dcfd9db06
maxtian-xuanlan <mxt6232996077@gmail.com>
2026-04-01 03:21:53 -0700

    Add missing import

--- src/omni_python_library/middleware/__init__.py
+++ src/omni_python_library/middleware/__init__.py
@@ -5,6 +5,7 @@ from omni_python_library.middleware.user_token import (
     get_user_roles,
     validate_create_permission,
 )
+from omni_python_library.middleware.header_logger import AuthHeaderLoggingMiddleware
 
 __all__ = [
     "get_current_user",
@@ -12,4 +13,5 @@ __all__ = [
     "get_user_roles",
     "get_user_context",
     "validate_create_permission",
+    "AuthHeaderLoggingMiddleware",
 ]
