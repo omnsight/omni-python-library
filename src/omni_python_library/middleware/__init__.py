8a85cfa5b58faed64037b2301d1667bf007a0ec0
bouncingmaxt <212165011+bouncingmaxt@users.noreply.github.com>
2026-04-01 10:23:03 +0000

    Auto commit: update dependencies & generated client & code formatting

--- src/omni_python_library/middleware/__init__.py
+++ src/omni_python_library/middleware/__init__.py
@@ -1,3 +1,4 @@
+from omni_python_library.middleware.header_logger import AuthHeaderLoggingMiddleware
 from omni_python_library.middleware.user_token import (
     get_current_user,
     get_owner_from_token,
@@ -5,7 +6,6 @@ from omni_python_library.middleware.user_token import (
     get_user_roles,
     validate_create_permission,
 )
-from omni_python_library.middleware.header_logger import AuthHeaderLoggingMiddleware
 
 __all__ = [
     "get_current_user",
